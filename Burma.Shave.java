051cc7f95e0eefb9b06454fb34a16aad0e50bab5
lungilesibuyi25085740 <25085740@mvula.univen.ac.za>
2025-04-04 13:13:10 +0200

    Update Burma.Shave.java

--- Burma.Shave.java
+++ Burma.Shave.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Burma.Shave{
 
-public static void main( String {} args) 
+public static void main( String {} args); 
 String name,noun 1, noun 2, noun 3, noun 4;
 System.out.print ("Shaving brushes, You'll soon see 'em,In some museum,Burma Shave");
